eec7b26beca3d5529f56f71d9ab015ee40f68bea
Sarthak Narain <sarthaknarain27@gmail.com>
2023-01-24 10:54:12 +0530

    optimized method added

--- LeetCode/merge intervals.cpp
+++ LeetCode/merge intervals.cpp
@@ -41,3 +41,30 @@ public:
     }
 };  
 */
+
+// BEST SOLUTION->
+
+/*
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        ans.push_back(intervals[0]);
+        int j=0;
+
+        for(int i=1;i<intervals.size();i++){
+            if(ans[j][1] >= intervals[i][0]){
+                if(ans[j][1] <= intervals[i][1]){
+                    ans[j][1] = intervals[i][1];
+                }
+            }
+            else{
+                ans.push_back(intervals[i]);
+                j++;
+            }
+        }
+        return ans; 
+    }
+};
+*/
