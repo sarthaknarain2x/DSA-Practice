0aa791d1307e7aa6ff8e4414d27b1d2ce784b824
Sarthak Narain <sarthaknarain27@gmail.com>
2023-01-29 01:51:56 +0530

    Create single element in a sorted array.cpp

---
+++ LeetCode/single element in a sorted array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int lo = 0, hi = nums.size()-1, mid;
+        while(lo<hi) {
+            mid = lo+(hi-lo)/2;
+            if(mid%2==0 && nums[mid]==nums[mid+1] || mid%2==1 && nums[mid]==nums[mid-1]){
+                lo = mid+1;
+            }
+            else{
+                hi = mid;
+            }
+        }
+        return nums[lo];
+    }
+};
