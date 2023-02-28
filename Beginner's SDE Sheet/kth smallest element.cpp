b8466c6e851b454555d1a3ed6594d7f372cff16e
Sarthak Narain <sarthaknarain27@gmail.com>
2023-02-28 17:09:20 +0530

    max heap solution added

--- Beginner's SDE Sheet/kth smallest element.cpp
+++ Beginner's SDE Sheet/kth smallest element.cpp
@@ -1,3 +1,12 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
 class Solution{
     public:
     // arr : given array
@@ -6,32 +15,39 @@ class Solution{
     // k : find kth smallest element and return using this function
     int kthSmallest(int arr[], int l, int r, int k) {
         //code here
-        priority_queue <int> pq;
+        priority_queue <int> pq; // max heap
         
-        for(int i=0; i<k; i++)
-            pq.push(arr[i]);
+        for(int i=l; i<=r; i++) {
+            pq.push(arr[i]); // push elements in array
             
-        for(int i=k; i<=r; i++) {
-            if(arr[i] < pq.top()) {
-                pq.pop();
-                pq.push(arr[i]);
+            if(pq.size() > k){ // whenever size of max heap becomes greater than k
+                pq.pop(); // pop the top element. 
             }
         }
-        return pq.top();
+        return pq.top(); // after traversing the array, the top element will be our answer
     }
 };
 
-// another better approach:
-/*int kthSmallest(int arr[], int l, int r, int k) {
-        //code here
-        priority_queue <int> pq;
+//{ Driver Code Starts.
+ 
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
         
-        for(int i=l; i<=r; i++)
-            pq.push(arr[i]);
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
             
-        while(pq.size() > k)
-            pq.pop();
-            
-        return pq.top();
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
     }
-*/
+    return 0;
+}
+// } Driver Code Ends
