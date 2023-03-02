8fa8850a2d1659fd4110a91860f3530f1a43f8cc
Sarthak Narain <sarthaknarain27@gmail.com>
2023-03-02 15:04:02 +0530

    min heap solution added

--- Beginner's SDE Sheet/kth largest sum contiguous subarray.cpp
+++ Beginner's SDE Sheet/kth largest sum contiguous subarray.cpp
@@ -1,16 +1,20 @@
 class Solution{
 public:
     int kthLargest(vector<int> &Arr,int N,int K){
-        vector<int> arr;
+        priority_queue <int, vector<int>, greater <int>> minh;
         
         for(int i=0; i<N; i++) {
             int sum = 0;
             for(int j=i; j<N; j++) {
                 sum += Arr[j];
-                arr.push_back(sum);
+                minh.push(sum);
+                if(minh.size() > K) {
+                    minh.pop();
+                }
             }
         }
-        sort(arr.begin(), arr.end());
-        return arr[arr.size()-K];
+        
+        int ans = minh.top();
+        return ans;
     }
 };
