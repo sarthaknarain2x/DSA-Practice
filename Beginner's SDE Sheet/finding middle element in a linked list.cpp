1aef53519617010429930fc59c9bf8a0dc4208db
Sarthak Narain <sarthaknarain27@gmail.com>
2023-03-16 10:31:16 +0530

    Update finding middle element in a linked list.cpp

--- Beginner's SDE Sheet/finding middle element in a linked list.cpp
+++ Beginner's SDE Sheet/finding middle element in a linked list.cpp
@@ -1,3 +1,5 @@
+
+// better soln
 class Solution{
     public:
     /* Should return data of middle node. If linked list is empty, then  -1*/
@@ -5,15 +7,34 @@ class Solution{
     {
         // Your code here
         Node* moveOnce = head;
-        Node* moveTwice = head->next;
+        Node* moveTwice = head;
         
-        while(moveTwice!=NULL) {
-            moveTwice = moveTwice->next;
-            if(moveTwice!=NULL) {
-                moveTwice = moveTwice->next;
-            }
+        while(moveTwice!= NULL && moveTwice->next!=NULL) {
             moveOnce = moveOnce->next;
+            moveTwice = moveTwice->next->next;
         }
         return moveOnce->data;
     }
 };
+
+//another way same concept
+
+// class Solution{
+//     public:
+//     /* Should return data of middle node. If linked list is empty, then  -1*/
+//     int getMiddle(Node *head)
+//     {
+//         // Your code here
+//         Node* moveOnce = head;
+//         Node* moveTwice = head->next;
+        
+//         while(moveTwice!=NULL) {
+//             moveTwice = moveTwice->next;
+//             if(moveTwice!=NULL) {
+//                 moveTwice = moveTwice->next;
+//             }
+//             moveOnce = moveOnce->next;
+//         }
+//         return moveOnce->data;
+//     }
+// };
