a698fe86aafc194cf7d8185be451507cdef62229
Sarthak Narain <sarthaknarain27@gmail.com>
2023-04-08 15:48:00 +0530

    0ms soln added (digital root)

--- LeetCode/add digits.cpp
+++ LeetCode/add digits.cpp
@@ -15,3 +15,20 @@ public:
         return num;
     }
 };
+
+/*0ms Solution->
+If number is divisible by 9 completely then digital root(ans) is 9,else the digital root is remainder obtained.
+class Solution {
+public:
+    int addDigits(int num) {
+        int ans;
+        if(num<9)
+            return num;
+        if(num%9==0)
+            ans = 9;
+        else
+            ans = num%9;
+        return ans;
+    }
+};
+*/
