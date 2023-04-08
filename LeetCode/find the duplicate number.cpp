b4409fef1ab927bbf29c8c86ac864faa1f1bf33f
Sarthak Narain <sarthaknarain27@gmail.com>
2023-04-08 14:43:01 +0530

    better binary search soln added

--- LeetCode/find the duplicate number.cpp
+++ LeetCode/find the duplicate number.cpp
@@ -1,4 +1,28 @@
+// better binary search solution
 class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int lo = 0, hi = nums.size()-1, cnt=0, mid;
+        while(lo<=hi) {
+            cnt=0;
+            mid = lo+(hi-lo)/2;
+            for(int n: nums) {
+                if(n<=mid) {
+                    cnt++;
+                }
+            }
+            if(cnt<=mid) {
+                lo = mid+1;
+            }
+            else {
+                hi = mid-1;
+            }
+        }
+        return lo;
+    }
+};
+// XOR soln
+/*class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
     sort(nums.begin(),nums.end());
@@ -13,3 +37,4 @@ public:
     return ans;
     }
 };
+*/
