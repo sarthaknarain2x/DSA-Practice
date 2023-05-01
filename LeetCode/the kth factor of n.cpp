104b84ffbbaed7663aa3334d161a9c51c5627664
Sarthak Narain <sarthaknarain27@gmail.com>
2023-05-01 13:21:17 +0530

    Create the kth factor of n.cpp

---
+++ LeetCode/the kth factor of n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int> factors;
+        for(int i=1; i<=n; i++) {
+            if(n%i==0) {
+                factors.push_back(i);
+            }
+        }
+        if(factors.size() < k) {
+            return -1;
+        }
+        else {
+            return factors[k-1];
+        }
+    }
+};
