dec86067056e3d5ddef49db8c878c86476885c31
Sarthak Narain <sarthaknarain27@gmail.com>
2023-05-05 22:18:46 +0530

    Create maximum number of vowels in a substring of given length.cpp

---
+++ LeetCode/maximum number of vowels in a substring of given length.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public static boolean check(char ch) {
+        return (ch == 'a' || ch == 'e' || ch == 'i' | ch == 'o' || ch == 'u');
+    }
+    public int maxVowels(String s, int k) {
+        int max = 0, n = s.length();
+        int count = 0;
+        for(int i = 0; i < k; i++) {
+            if(check(s.charAt(i))) count++;
+        }
+        max = count;
+		// build first window size k
+        for(int  i = k; i < n; i++) {
+			// remove the contribution of the (i - k)th character which is no longer in the window
+            if(check(s.charAt(i - k))) count--;
+			// add the contribution of the current character
+            if(check(s.charAt(i))) count++;
+			// update max at for each window of size k
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+}
