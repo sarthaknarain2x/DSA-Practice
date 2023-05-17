074dfd6d0016a43b66ec183b55bea098e0c0eae7
Sarthak Narain <sarthaknarain27@gmail.com>
2023-05-17 14:01:54 +0530

    Create delete greatest value in each row.cpp

---
+++ LeetCode/delete greatest value in each row.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int deleteGreatestValue(vector<vector<int>>& grid) {
+        int row = grid[0].size();
+        int col = grid.size();
+
+        int ans=0, maxi=0;
+
+        for(int i=0; i<row; i++) {
+            sort(grid[i].begin(), grid[i].end());
+        }
+
+        for(int i=0; i<col; i++) {
+            for(int j=i; j<row; j++) {
+                maxi = max(maxi, grid[j][i]);
+            }
+            ans += maxi;
+        }
+        return ans;
+    }
+};
