3d2b6f826047d01001e522c0cd44003fdbe6c3e2
Sarthak Narain <sarthaknarain27@gmail.com>
2023-06-11 11:34:52 +0530

    set solution added

--- LeetCode/check if the number is fascinating.cpp
+++ LeetCode/check if the number is fascinating.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     bool isFascinating(int n) {
-        return n == 192 || n == 219 || n==273 || n==327;
+        string str = to_string(n) + to_string(n*2) + to_string(n*3);
+        set <char> s(str.begin(), str.end());
+        if(s.size()==9 && str.size()==9 && s.find('0') == s.end())
+            return true;
+        else
+            return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool isFascinating(int n) {
+//         return n == 192 || n == 219 || n==273 || n==327;
+//     }
+// };
