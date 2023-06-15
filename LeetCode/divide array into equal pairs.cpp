dbf817368509d0ce1f87190430f31b6429c6b1b7
Sarthak Narain <sarthaknarain27@gmail.com>
2023-06-15 08:29:29 +0530

    Create divide array into equal pairs.cpp

---
+++ LeetCode/divide array into equal pairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++) {
+            mp[nums[i]]++;
+        }
+        for(auto itr: mp) {
+            if(itr.second % 2 != 0)
+                return false;
+        }
+        return true;
+    }
+};
