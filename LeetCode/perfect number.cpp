4636957d294290e3fc1ddc488a4058da2b2d665a
Sarthak Narain <sarthaknarain27@gmail.com>
2023-06-16 14:11:25 +0530

    Create perfect number.cpp

---
+++ LeetCode/perfect number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num==1)
+            return false;
+        int sum=0;
+        for(int i=1; i<=num/2; i++) {
+            if(num%i == 0) {
+                sum += i;
+            }
+        }
+        return (sum==num);
+    }
+};
