51745e97f8e26b533552e6d45256602e4cf640ba
Sarthak Narain <sarthaknarain27@gmail.com>
2023-06-28 22:08:24 +0530

    Create Number of Steps to reduce a number to zero.cpp

---
+++ LeetCode/Number of Steps to reduce a number to zero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int c=0;
+        while(num!=0) {
+            if(num%2==0) {
+                num = num/2;
+                c++;
+            }
+            else if(num%2 != 0) {
+                num = num-1;
+                c++;
+            }
+        }
+        return c;
+    }
+};
