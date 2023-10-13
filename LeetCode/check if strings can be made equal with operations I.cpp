2a6a8dffb172d98d40c2c8798da77263b68b4503
Sarthak Narain <sarthaknarain27@gmail.com>
2023-10-13 10:10:30 +0530

    Create check if strings can be made equal with operations I.cpp

---
+++ LeetCode/check if strings can be made equal with operations I.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+
+        if(s1==s2) 
+            return true;
+
+        int i=0; int j=2;
+
+        while(i<4){
+            if(s1[i] == s2[i]){
+                i++; 
+                j++; 
+                continue;
+            }
+            else {
+                if(s1[i] != s2[j]) {
+                    return false; 
+                }
+                else {
+                    swap(s2[i], s2[j]);
+                    i++; 
+                    j++; 
+                }
+            }
+        }
+        return true;
+    }
+};
