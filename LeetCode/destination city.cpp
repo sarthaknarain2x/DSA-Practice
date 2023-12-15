bd36d275130e9e13240eb167890dfe8bdf93c58f
Sarthak Narain <sarthaknarain27@gmail.com>
2023-12-15 13:56:22 +0530

    Create destination city.cpp

---
+++ LeetCode/destination city.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        int n = paths.size();
+        
+        unordered_map<string, int> mp;
+        for(int i = 0; i < n; i++) {
+            mp[paths[i][0]]++;
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(mp[paths[i][1]] <= 0)
+                return paths[i][1];
+        }
+        return "";   
+    }	
+};
